53826b69b5fe63c383c79f20c8229d5bfaaf13c8
CamiPle <camipletscher@gmail.com>
2023-01-18 14:36:38 -0300

    home responsive css

--- cami-pletscher/src/Components/home/Home.jsx
+++ cami-pletscher/src/Components/home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
           </div>
           <div className={styles.cont_description}>
             <p>
-              Soy Cami y te invito a que me <br></br> conozcas a mi y a mi
+              Te invito a que me <br></br> conozcas a mi y a mi
               trabajo.
             </p>
           </div>
@@ -34,8 +34,12 @@ function Home() {
         </div>
       </div>
       <div className={styles.container_profile}>
-        <img
+        <img className={styles.hide}
           src="https://i.postimg.cc/W1NByrWk/selfie-fondogris.png"
+          alt=""
+        ></img>
+        <img className={styles.show}
+          src="https://i.postimg.cc/gkwZmJNB/selfie-fondo-cir.png"
           alt=""
         ></img>
       </div>
