36befe73a571c4f181cfe5bcc09d7ada458ed6e2
CamiPle <camipletscher@gmail.com>
2023-01-18 00:58:54 -0300

    home css

--- cami-pletscher/src/Components/home/Home.jsx
+++ cami-pletscher/src/Components/home/Home.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
-import styles from './home.module.css'
+import React from "react";
+import styles from "./home.module.css";
 
 function Home() {
   return (
     <div className={styles.container_home}>
-        <div className={styles.container_logo_txt}>
-            <div className={styles.container_logo}>
-                <img src='https://i.postimg.cc/vZP57FxV/Principal.png' alt=''></img>
-            </div>
-            <div>
-                <div>HOLA!</div>
-                <div>SOY CAMILA</div>
-                <div>Graphic Designer & Front end developer</div>
-                <div>Soy Cami y te invito a que me conozcas a mi y a mi trabajo.</div>
-            </div>
-            <div>
-                <button>Descargar cv</button>
-                <button>Contacto</button>
-            </div>
-            <div>
-                <button>^</button>
-            </div>
+      <div className={styles.container_logo_txt}>
+        <div className={styles.container_logo}>
+          <img src="https://i.postimg.cc/vZP57FxV/Principal.png" alt=""></img>
         </div>
-        <div className={styles.container_profile}>
-            <img src='https://i.postimg.cc/W1NByrWk/selfie-fondogris.png' alt=''></img>
+        <div className={styles.container_txt}>
+          <div className={styles.cont_hola}>
+            <p>HOLA!</p>
+          </div>
+          <div className={styles.cont_soy}>
+            <p>
+              SOY <span>CAMILA</span>
+            </p>
+          </div>
+          <div className={styles.cont_anclaje}>
+            <p>Graphic Designer & Front end developer</p>
+          </div>
+          <div className={styles.cont_description}>
+            <p>
+              Soy Cami y te invito a que me <br></br> conozcas a mi y a mi
+              trabajo.
+            </p>
+          </div>
         </div>
+        <div className={styles.container_buttons}>
+          <button className={styles.download}>Descargar cv</button>
+          <button className={styles.contact}>Contacto</button>
+          <button className={styles.start}><i className="large material-icons">keyboard_arrow_down</i></button>
+        </div>
+      </div>
+      <div className={styles.container_profile}>
+        <img
+          src="https://i.postimg.cc/W1NByrWk/selfie-fondogris.png"
+          alt=""
+        ></img>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
