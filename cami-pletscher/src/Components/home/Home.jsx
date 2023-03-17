4ef03af0b37fece02228e81c638b061c7ab1e8bf
CamiPle <camipletscher@gmail.com>
2023-03-17 01:48:02 -0300

    cambio de imagen

--- cami-pletscher/src/Components/home/Home.jsx
+++ cami-pletscher/src/Components/home/Home.jsx
@@ -4,18 +4,17 @@ import styles from "./home.module.css";
 
 function Home() {
   return (
-    <div className={styles.container_home}>
-      <div className={styles.container_logo_txt}>
-        <div className={styles.container_logo}>
-          <img src="https://i.postimg.cc/vZP57FxV/Principal.png" alt=""></img>
-        </div>
-        <div className={styles.profile_mobile}>
-          <img
-            className={styles.show}
-            src="https://i.postimg.cc/gkwZmJNB/selfie-fondo-cir.png"
-            alt=""
-          ></img>
-        </div>
+  <div>
+    <div>
+      <div className={styles.container_logo}>
+        <img src="https://i.postimg.cc/vZP57FxV/Principal.png" alt=""></img>
+      </div>
+      <div>
+        NAV
+      </div>
+    </div>
+    <div>
+    <div className={styles.container_izq}>
         <div className={styles.container_txt}>
           <div className={styles.cont_hola}>
             <p>HOLA!</p>
@@ -35,21 +34,23 @@ function Home() {
         <div className={styles.container_buttons}>
           <button className={styles.download}>Descargar cv</button>
           <button className={styles.contact}>Contacto</button>
-          <Link to="/about_me">
-            <button className={styles.start}>
-              <i className="large material-icons">keyboard_arrow_down</i>
-            </button>
-          </Link>
+
         </div>
       </div>
       <div className={styles.container_profile}>
         <img
           className={styles.hide}
-          src="https://i.postimg.cc/W1NByrWk/selfie-fondogris.png"
+          src="https://i.postimg.cc/ZYV3MM3x/home.png"
           alt=""
         ></img>
       </div>
     </div>
+  </div>
+
+
+
+
+
   );
 }
 
