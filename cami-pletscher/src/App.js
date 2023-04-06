1515ad67d2cfde6d22f979ccbca96a33b8a4c350
CamiPle <camipletscher@gmail.com>
2023-04-06 12:35:35 -0300

    ajustes

--- cami-pletscher/src/App.js
+++ cami-pletscher/src/App.js
@@ -1,15 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Container from "./Components/Container";
-import Home from "./Components/home/Home";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/start" element={<Container/>}></Route>
+        <Route path="/" element={<Container />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
